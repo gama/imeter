76ddc263399aa6e61ab4339ec7b5a508cba72a89
Gustavo Gama <gustavo.gama@gmail.com>
2019-01-21 19:45:53 -0200

    Enable "css-modules" for custom styles
    But preserve "global" names for the Bulma styles.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-const withPreact = require('@zeit/next-preact')
-const withSass   = require('@zeit/next-sass')
+const getLocalIdent = require('css-loader/lib/getLocalIdent')
+const withPreact    = require('@zeit/next-preact')
+const withSass      = require('@zeit/next-sass')
 
 module.exports = withPreact(withSass({
-    webpack(config, options) {
-        // further custom configuration here
-        return config
+    cssLoaderOptions: {
+        modules:        true,
+        importLoaders:  1,
+        localIdentName: "[local]___[hash:base64:5]",
+        getLocalIdent:  (loaderContext, localIdentName, localName, options) => {
+            return loaderContext.resourcePath.includes('.global.') ?
+                   localName :
+                   getLocalIdent(loaderContext, localIdentName, localName, options);
+        }
     }
 }))
